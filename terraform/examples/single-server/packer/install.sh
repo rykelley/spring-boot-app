e353d5b248ae766efe388495d2c03c07c3611482
Ryan Kelley <rykelley@gmail.com>
2019-10-13 00:44:45 -0600

    last commit

--- terraform/examples/single-server/packer/install.sh
+++ terraform/examples/single-server/packer/install.sh
@@ -6,7 +6,7 @@ sudo apt install docker.io
 sudo systemctl start docker
 sudo apt-get install openjdk-11-jdk
 sudo apt-get install maven
-sudo git clone https://github.com/rykelley/spring-boot-app.git /var/www/app
- /var/www/app/ mvn -N io.takari:maven:wrapper
-./mvnw -Pprod clean verify
-java -jar target/*.jar
+sudo git clone https://github.com/rykelley/spring-boot-app.git /home/ubuntu
+/home/ubuntu/ mvn -N io.takari:maven:wrapper
+/home/ubuntu/ ./mvnw -Pprod clean verify
+/home/ubuntu/ java -jar target/*.jar
